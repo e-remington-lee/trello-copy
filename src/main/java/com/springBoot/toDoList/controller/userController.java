aa6740540f73189a60fb7116d43a5fef03291365
Erik Remington Lee <eriklee19@gmail.com>
2019-09-16 20:21:09 -0400

    serving index.html file from angular

--- src/main/java/com/springBoot/toDoList/controller/userController.java
+++ src/main/java/com/springBoot/toDoList/controller/userController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Collection;
 
 @RestController
+//@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/api/users")
 public class userController {
 
