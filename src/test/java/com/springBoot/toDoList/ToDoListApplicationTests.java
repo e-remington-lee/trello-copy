7aeed0f88e98b1e01148920f2bf1757496144933
Erik Remington Lee <eriklee19@gmail.com>
2019-10-06 22:00:16 -0400

    adding automated testing

--- src/test/java/com/springBoot/toDoList/ToDoListApplicationTests.java
+++ src/test/java/com/springBoot/toDoList/ToDoListApplicationTests.java
@@ -2,6 +2,8 @@ package com.springBoot.toDoList;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,6 +13,9 @@ public class ToDoListApplicationTests {
 
 	@Test
 	public void contextLoads() {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\erikl\\Downloads\\chromedriver_win32\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://www.e-remington-lee.com/");
+	driver.manage().window().maximize();
 	}
-
 }
